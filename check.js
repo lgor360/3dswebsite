51896a8d235dbeda3eac48a49f13b880f226fa31
Игорь <93474580+lgor360@users.noreply.github.com>
2024-08-25 19:20:14 +0300

    Create check.js

---
+++ check.js
@@ -0,0 +1,7 @@
+if (navigator.platform === "New Nintendo 3DS") {
+  console.log("тест прошёл!");
+} else if (navigator.platform === "Nintendo 3DS") {
+  console.log("тест прошёл!");
+} else {
+  location.href = "https://lgor360.github.io/igor360website/home.html";
+}
